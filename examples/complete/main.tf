fe3e9a3a00aae3f94ec53a6bec561e792384a15c
Dan Meyers <danjbh@users.noreply.github.com>
2022-07-29 08:22:17 -0700

    Updating terratest

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,7 +1,18 @@
+module "s3_bucket" {
+  source  = "cloudposse/s3-bucket/aws"
+  version = "2.0.3"
+
+  enabled                  = true
+  acl                      = "private"
+  versioning_enabled       = false
+
+  context = module.this.context
+}
+
 module "example" {
   source = "../.."
 
-  s3_bucket_arn = "foo"
+  s3_bucket_arn = module.s3_bucket.bucket_arn
   lf_tags       = var.lf_tags
   resources     = var.resources
 
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,4 +1,9 @@
+output "s3_bucket_id" {
+  description = "Name of S3 bucket created to store data."
+  value       = module.s3_bucket.bucket_id
+}
+
 output "lf_tags" {
   description = "List of LF tags created."
-  value       = aws_lakeformation_lf_tag.default
+  value       = module.example.lf_tags
 }
