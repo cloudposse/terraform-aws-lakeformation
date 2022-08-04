b8c304771725f4de89278978bcb9486c8bce6a47
Dan Meyers <20687081+danjbh@users.noreply.github.com>
2022-08-04 08:25:14 -0700

    Update examples/complete/variables.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -2,7 +2,7 @@ variable "region" {
   type = string
 }
 
-variable "lf_tags" {
+variable "lakeformation_tags" {
   description = "A map of key-value pairs to be used as Lake Formation tags."
   type        = map(list(string))
   default     = {}
