db1e7560c4854f35006c797017391fd468689686
Dan Meyers <20687081+danjbh@users.noreply.github.com>
2022-08-04 08:21:59 -0700

    Update examples/complete/outputs.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -3,7 +3,7 @@ output "s3_bucket_id" {
   value       = module.s3_bucket.bucket_id
 }
 
-output "lf_tags" {
+output "lakeformation_tags" {
   description = "List of LF tags created."
   value       = module.example.lf_tags
 }
